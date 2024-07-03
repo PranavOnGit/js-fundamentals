8bbdc98571eee0a269675addcc91a0cd22eae81e
Pranav Chavan <Pranav.chavan@alliedanalytics.com>
2024-07-03 17:44:16 +0530

    forin

--- module-3/while.js
+++ module-3/while.js
@@ -1,10 +1,20 @@
 
 //We have to print 1 to 10
 
-var i = 1 ; var j = 10
 
+//WHILE loop
+var i = 1 ; var j = 10
 while(i <= j)
 {
     console.log(i);
     i++
 }
+
+//Do While Loop
+var l = 5 ; var m = 10
+
+do{
+    console.log(l)
+    l++
+}while( l <= m)
+
