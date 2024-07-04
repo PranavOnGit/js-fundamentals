3c80e6300f2b1d91a2d5ba2e75c431ea5913c47a
Pranav Chavan <tasksontable@gmail.com>
2024-07-04 09:02:56 +0530

    functions with expression

---
+++ module-4/functionExp.js
@@ -0,0 +1,17 @@
+
+
+// var a = 2 + 3  EXPRESSION
+
+
+//Function without a name is anonymus function
+
+var add = function sum(a, b)
+{
+    return 2 + 3
+}
+
+var sum = add(2, 3)
+
+var sumPlus  = sum + 1
+
+console.log(sumPlus)
