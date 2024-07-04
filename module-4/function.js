bcc7f658512df08f97ac0fd7fe59feff204252ff
Pranav Chavan <tasksontable@gmail.com>
2024-07-04 08:37:20 +0530

    module-4

---
+++ module-4/function.js
@@ -0,0 +1,16 @@
+
+
+//Normal Function
+function carOne() {
+    console.log('calliing simple function!')
+}
+
+carOne()
+
+//Parametrized Function
+function car(params) {
+    console.log('This is parameterized function! COLOR : ', params)
+}
+
+
+car('RED')
